a46637bb6a743ff349eaad37ce40a016160fa95c
Esamatti Kakela <esamatti.kakela@tieto.com>
2021-12-15 13:55:32 +0200

    script finetuning

--- scripts/authorize_client.sh
+++ scripts/authorize_client.sh
@@ -3,4 +3,4 @@ set -x
 
 aws ec2 authorize-client-vpn-ingress --client-vpn-endpoint-id $CLIENT_VPN_ID --target-network-cidr $TARGET_CIDR --authorize-all-groups
 
-aws ec2  apply-security-groups-to-client-vpn-target-network--client-vpn-endpoint-id $CLIENT_VPN_ID --vpc-id $VPC_ID --security-group-ids $SG_ID
+aws ec2  apply-security-groups-to-client-vpn-target-network --client-vpn-endpoint-id $CLIENT_VPN_ID --vpc-id $VPC_ID --security-group-ids $SG_ID
